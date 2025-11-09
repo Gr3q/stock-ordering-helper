564882f5368e773a3358f2a929037127440df3e8
Attila Greguss <floyd0122@gmail.com>
2025-11-09 17:21:30 +0000

    make sure hitting enter works

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,67 +21,75 @@
 
 <div class="w-lvw h-lvh p-2 bg-gray-900 text-gray-50">
   <h1>Stock search</h1>
-  <div class="flex row gap-2">
-    {#snippet clearButton()}
-      <IconButton
-        style="z-index: 10;"
-        disabled={searchInput.trim() === ""}
-        onclick={() => {
-          searchInput = "";
-        }}><Icon icon="mdi:close" /></IconButton
-      >
-    {/snippet}
-    <TextField
-      class="input"
-      type="text"
-      value={searchInput}
-      oninput={(e) => {
-        if (!(e.target instanceof HTMLInputElement)) {
-          return;
-        }
-
-        searchInput = e.target.value;
-      }}
-      placeholder="mozzarella"
-      endAdornment={clearButton}
-    />
-    <Button
-      disabled={searchInput.trim() === ""}
-      onclick={() => {
-        search = searchInput;
-        for (const site of Sites) {
-          if (!sitesToOpen?.has(site)) {
-            continue;
+  <form
+    onsubmit={(e) => {
+      e.preventDefault();
+    }}
+  >
+    <div class="flex row gap-2">
+      {#snippet clearButton()}
+        <IconButton
+          style="z-index: 10;"
+          disabled={searchInput.trim() === ""}
+          type="reset"
+          onclick={() => {
+            searchInput = "";
+          }}><Icon icon="mdi:close" /></IconButton
+        >
+      {/snippet}
+      <TextField
+        class="input"
+        type="text"
+        value={searchInput}
+        oninput={(e) => {
+          if (!(e.target instanceof HTMLInputElement)) {
+            return;
           }
 
-          const siteInfo = SiteConfigs[site];
-          const url = constructUrl(siteInfo.searchUrl, {
-            ...siteInfo.additionalParams,
-            ...(search ? { [siteInfo.searchQueryParam]: search } : {}),
-          });
-          window.open(url.toString(), "_blank");
-        }
-      }}>Open selected</Button
-    >
-  </div>
-  {#each Sites as site (site)}
-    <Site
-      {site}
-      search={searchInput}
-      checked={sitesToOpen ? sitesToOpen.has(site) : null}
-      onChecked={(checked) => {
-        if (!sitesToOpen) {
-          return;
-        }
+          searchInput = e.target.value;
+        }}
+        placeholder="mozzarella"
+        endAdornment={clearButton}
+      />
+      <Button
+        type="submit"
+        disabled={searchInput.trim() === ""}
+        onclick={() => {
+          search = searchInput;
+          for (const site of Sites) {
+            if (!sitesToOpen?.has(site)) {
+              continue;
+            }
 
-        if (checked) {
-          sitesToOpen.add(site);
-        } else {
-          sitesToOpen.delete(site);
-        }
+            const siteInfo = SiteConfigs[site];
+            const url = constructUrl(siteInfo.searchUrl, {
+              ...siteInfo.additionalParams,
+              ...(search ? { [siteInfo.searchQueryParam]: search } : {}),
+            });
+            window.open(url.toString(), "_blank");
+          }
+        }}>Open selected</Button
+      >
+    </div>
+    {#each Sites as site (site)}
+      <Site
+        {site}
+        search={searchInput}
+        checked={sitesToOpen ? sitesToOpen.has(site) : null}
+        onChecked={(checked) => {
+          if (!sitesToOpen) {
+            return;
+          }
 
-        saveToStorage("sites", [...sitesToOpen]);
-      }}
-    />
-  {/each}
+          if (checked) {
+            sitesToOpen.add(site);
+          } else {
+            sitesToOpen.delete(site);
+          }
+
+          saveToStorage("sites", [...sitesToOpen]);
+        }}
+      />
+    {/each}
+  </form>
 </div>
